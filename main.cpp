fee56ffa01b86df60fccf8ed7e1be9e0beeee7c2
mauricio <mcasals@fie.undef.edu.ar>
2023-10-04 12:17:18 -0300

    prueba de commit dentro de la rama

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Este deberia ser la rama 'mauri'" << std::endl;
+    std::cout << "Este deberia ser la rama 'mauricio'" << std::endl;
+
     return 0;
 }
